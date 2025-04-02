e53f12ba589f9a96e1789164ee5b52e6415a5ed4
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-04-02 20:27:49 +0200

    added whole lab

--- server.js
+++ server.js
@@ -1,40 +1,34 @@
-/*
-  📦 Dependy the Importer  
-  Zaimportuj wszystkie wymagane moduły: path, express, body-parser, logger oraz routing.  
-*/
-const http = require("http");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { STATUS_CODE } = require("./constants/statusCode");
+const { getInfoLog, getErrorLog } = require("./utils/logger");
+
 const config = require("./config");
-const { requestRouting } = require("./routing/routing");
+const prRouters = require("./routing/product");
+const logOutRoute = require("./routing/logout");
+const killRoute = require("./routing/kill");
+const Home = require("./routing/home");
 
-const requestListener = (request, response) => {
-  requestRouting(request, response);
-};
+const app = express();
 
-const server = http.createServer(requestListener);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(config.PORT);
+app.use((req, res, next) => {
+  console.log(getInfoLog(req.method, req.url));
+  next();
+});
 
-/*
-  🏗 Structo the Builder  
-  Utwórz instancję aplikacji express i zapisz ją w stałej app.  
-*/
-/*
-  🏗 Structo the Builder  
-  Zarejestruj middleware body-parser do parsowania ciał formularzy. 
-*/
-/*
-  🏗 Structo the Builder  
-  Dodaj middleware logujący informacje o każdym przychodzącym żądaniu.  
-*/
-/*
-  🏗 Structo the Builder  
-  Zarejestruj middleware obsługujące poszczególne ścieżki.  
-*/
-/*
-  🏗 Structo the Builder  
-    Obsłuż stronę 404 – zwróć plik 404.html i zaloguj błąd.   
-*/
-/*
-  🏗 Structo the Builder  
-    Uruchom serwer i nasłuchuj na porcie z config.js.    
-*/
+app.use("/product", prRouters);
+app.use("/logout", logOutRoute);
+app.use("/kill", killRoute);
+app.use("/", Home);
+
+app.use((req, res) => {
+  res
+    .status(STATUS_CODE.NOT_FOUND)
+    .sendFile(path.join(__dirname, "views", "404.html"));
+  console.warn(getErrorLog(req.url));
+});
+
+app.listen(config.PORT);
--- views/renderNewProductPage.js
+++ views/renderNewProductPage.js
@@ -1,5 +1,28 @@
-/* 🏗 Structo the Builder */
-/* Funkcja generująca stronę HTML z najnowszym produktem */
-const renderNewProductPage = () => {};
+const renderNewProductPage = (data) => {
+  let content = `
+    <html>
+    <head><title>Shop - Newest product</title></head>
+    <body>
+    <h1>Newest product</h1>
+    <nav>
+      <a href='/'>Home Page</a><br />
+      <a href='/product/add'>Add product</a><br />
+      <a href='/logout'>Logout</a>
+    </nav>
+  `;
+
+  if (!data) {
+    content += `<br /><div>No new products available.</div>`;
+  } else {
+    content += `<br /><div>New product data - ${data}</div>`;
+  }
+
+  content += `
+    </body>
+    </html>
+  `;
+
+  return content;
+};
 
 module.exports = renderNewProductPage;
